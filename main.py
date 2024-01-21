2d66bddf8247da0c680a426bd45c80d3a42fe56d
dasturchiuz <dasturchiuz@gmail.com>
2024-01-21 16:17:25 +0500

    v2

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, redirect, url_for, jsonify
 import face_recognition
+import cv2
+import numpy as np
+
 
 app = Flask(__name__)
 
@@ -13,18 +16,43 @@ def faceCompare():
         # FileStorage object wrapper
         if 'face1' not in request.files or 'face2' not in request.files:
             return jsonify({'error': 'Two face images are required.'}), 400
+        
+         # Faylni byte-larini olish
+        face2_bytes = request.files['face2'].read()
+
+        # Byte-larni NumPy arrayiga aylantirish
+        np_array = np.frombuffer(face2_bytes, np.uint8)
+
+        # NumPy arrayini tasvir sifatida o'qish
+        img = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
+
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        fm = cv2.Laplacian(gray, cv2.CV_64F).var()
+        print(fm)
+
+        if fm < 100: 
+            print('shart ishladi')
+            return jsonify({'error': 'Yuz sifati bilan muommo yuzaga keldi, iltimos kameraga yuzingizni qarating!'})
+        print("Nega bu qator ishladi")
+
 
         # Load face images
         image1 = face_recognition.load_image_file(request.files['face1'])
         image2 = face_recognition.load_image_file(request.files['face2'])
 
+        
+
         # Encode faces
         encoding1 = face_recognition.face_encodings(image1)
+        
         encoding2 = face_recognition.face_encodings(image2)
+        if not encoding2: 
+            return jsonify({'error': 'No face found FACE2'}), 400
+        
+        if not encoding1 :
+            return jsonify({'error': 'No face found FACE1'}), 400
 
-        if not encoding1 or not encoding2:
-            return jsonify({'error': 'No face found in one or both images.'}), 400
-
+        
         # Compare faces
         result = face_recognition.compare_faces([encoding1[0]], encoding2[0])
 
