1d7920e39268df2db09f6beb793d8bc808c38b35
dasturchiuz <dasturchiuz@gmail.com>
2024-01-29 10:34:06 +0500

    add threded

--- main.py
+++ main.py
@@ -63,4 +63,4 @@ def faceCompare():
 
 if __name__ == "__main__":
     # Please do not set debug=True in production
-    app.run(host="0.0.0.0", port=5000, debug=True )
+    app.run(host="0.0.0.0", port=5000, debug=True, threaded=True)
