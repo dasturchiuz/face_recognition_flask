6538d3eb994cfddf14d6c9ca37e944f384447626
dasturchiuz <dasturchiuz@gmail.com>
2024-02-08 11:34:27 +0500

    Flask

--- main.py
+++ main.py
@@ -13,42 +13,26 @@ def index():
 @app.route("/face-compare", methods=['POST'])
 def faceCompare():
     if request.method == 'POST':
-        # FileStorage object wrapper
-        if 'face1' not in request.files or 'face2' not in request.files:
-            return jsonify({'error': 'Two face images are required.'}), 400
-        
-         # Faylni byte-larini olish
-        face2_bytes = request.files['face2'].read()
-
-        # Byte-larni NumPy arrayiga aylantirish
-        np_array = np.frombuffer(face2_bytes, np.uint8)
-
-        # NumPy arrayini tasvir sifatida o'qish
+        face1 = request.form.get('face1')
+        face2 = request.form.get('face2')
+        face2_bytes = open(face2, 'rb')
+        np_array = np.frombuffer(face2_bytes.read(), np.uint8)
         img = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
-
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         fm = cv2.Laplacian(gray, cv2.CV_64F).var()
-
         if fm < 100: 
-            return jsonify({'error': 'Yuz sifati bilan muommo yuzaga keldi, iltimos kameraga yuzingizni qarating!'})
-
-
+            return {'error': 'Yuz sifati bilan muommo yuzaga keldi, iltimos kameraga yuzingizni qarating!'}
         # Load face images
-        image1 = face_recognition.load_image_file(request.files['face1'])
-        image2 = face_recognition.load_image_file(request.files['face2'])
-
-        
-
+        image1 = face_recognition.load_image_file(face1)
+        image2 = face_recognition.load_image_file(face2)
         # Encode faces
         encoding1 = face_recognition.face_encodings(image1)
-        
         encoding2 = face_recognition.face_encodings(image2)
         if not encoding2: 
-            return jsonify({'error': 'No face found FACE2'}), 400
+            return jsonify({'error': 'No face found FACE2'})
         
         if not encoding1 :
-            return jsonify({'error': 'No face found FACE1'}), 400
-
+            return jsonify({'error': 'No face found FACE1'})
         
         # Compare faces
         result = face_recognition.compare_faces([encoding1[0]], encoding2[0])
